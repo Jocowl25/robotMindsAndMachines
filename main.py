66a52b392dd832141b942b0de908e6cec76774fc
charles.agnew <agnewcm2015@gmail.com>
2025-12-01 15:24:34 -0500

    did the pathfinding and did the adding to safe spots list

--- main.py
+++ main.py
@@ -1,23 +1,42 @@
 import array
 import sys 
-new_limit = 200000 
-sys.setrecursionlimit(new_limit)
+#new_limit = 200000 
+#sys.setrecursionlimit(new_limit)
 class tile(object):
-    
-    def __init__(self,tuple_int_postion_temp,char_input_temp=" ", char_tile_state_temp=" "):
-        self.char_input = char_input_temp
-        self.char_tile_state = char_tile_state_temp
+    #constuctor
+    def __init__(self,tuple_int_postion_temp,arr_ping_temp=" ", arr_pings_on_temp=[],safe_temp =False):
+        self.arr_ping = arr_ping_temp
+        self.arr_pings_on = arr_pings_on_temp
         self.tuple_int_postion = tuple_int_postion_temp 
+        self.safe = safe_temp
+
     def print_tile(tile):
         print(" ("+str(tile.tuple_int_postion[0])+" , "+str(tile.tuple_int_postion[1])+") ")
+
+    def determine_tile_safeness(tile ,arr_new_ping_on):
+        if(tile.safe == True):
+            return
+        if( len(arr_new_ping_on) == 0):
+            tile.safe =True
+            tile.arr_pings_on = []
+            return
+            #add  wopus check to see if only one 
+            #add  coins check to see if only one 
+        if(len(tile.arr_pings_on) == 0 ):
+            tile.arr_pings_on = arr_new_ping_on             
+        else:
+
+            return
+        
+        
 
 first_row = [tile([0,0]),tile([0,1]),tile([0,2]),tile([0,3])]
 second_row = [tile([1,0]),tile([1,1]),tile([1,2]),tile([1,3])]
 third_row = [tile([2,0]),tile([2,1]),tile([2,2]),tile([2,3])]
 fourth_row = [tile([3,0]),tile([3,1]),tile([3,2]),tile([3,3])]
 total_board = [first_row,second_row,third_row,fourth_row]
+total_board[0][0].safe =True
 
-visited =[]
 
 for i in range(4):
     for j in range(4):
@@ -56,11 +75,14 @@ def next_square(rotation,tuplecurrentpostion):
             tuplecurrentpostion[0]=tuplecurrentpostion[0]-1
             return "completed"
     
+
+#takes input and return list of pings    
 def input_of_danger(current_postion):
-    global total_board
-    x = current_postion[0]
-    y =current_postion[1]
-    total_board[x][y].char_input = input("Give the danger level") 
+    print("at location " +str(current_postion[0])+ ","+str(current_postion[1]))
+    temp = input("Give the danger level").split()
+    print(len(temp))
+    return temp
+    
 
 
 
@@ -74,7 +96,9 @@ def plot_movement(current_postion,nextpostion,found,set_of_locations):
 
     temp = set_of_locations
     temp.append((cur_X,cur_y))
-    if(total_board[cur_X][cur_y].char_tile_state==" "):
+    #change to be checking for safe
+    if(total_board[cur_X][cur_y].safe==True):
+        #makes sure it leaves if it gets a good value aready
         if(found):
             return
         if(current_postion[0] == nextpostion[0] and current_postion[1] == nextpostion[1]):
@@ -107,7 +131,7 @@ def plot_movement(current_postion,nextpostion,found,set_of_locations):
                 
             
                     
-                
+            
             if(cur_y!=0 and (found ==False)):
                 if(cur_X,cur_y-1) in temp:
                     print()
@@ -117,6 +141,7 @@ def plot_movement(current_postion,nextpostion,found,set_of_locations):
                     
                 
     else:
+        temp.pop()
         return
 
 print("start of algorythm test")
@@ -126,14 +151,88 @@ for i in temp_solution:
     x= i[0]
     y=i[1]
     total_board[x][y].print_tile()
-    
-for i in total_board:
+
+
+
+safes =[(0,0)]
+print("start of printing test")  
+for i in range(4):
     currentline =""
-    for j in i:
-        pos = j.tuple_int_postion
+    for j in range(4):
+        pos = (j,i)
         if pos in temp_solution:
-            currentline = currentline+"X"
+            currentline = "X"+" "+currentline
         else:
-            currentline = currentline+" "
+            currentline = " "+" "+currentline
     print(currentline)
     
+def alocating_pings(current_postion):
+    global total_board
+    global safes
+    array_of_pings = input_of_danger(current_postion)
+    cur_X = current_postion[0]
+    cur_y = current_postion[1]
+    if((cur_X !=len(total_board)-1)):
+        next_x = cur_X+1
+        next_y = cur_y
+        total_board[next_x][next_y].determine_tile_safeness(array_of_pings)
+        print((total_board[next_x][next_y].safe))
+        if(total_board[next_x][next_y].safe ==True):
+            if (next_x,next_y) in safes:
+                print("is there")
+            else:
+                safes.append((next_x,next_y))
+
+    if(cur_y !=len(total_board[cur_X])-1):
+        next_x = cur_X
+        next_y = cur_y+1
+        total_board[next_x][next_y].determine_tile_safeness(array_of_pings)
+        print((total_board[next_x][next_y].safe))
+        if(total_board[next_x][next_y].safe ==True):
+            if (next_x,next_y) in safes:
+                print("is there")
+            else:
+                safes.append((next_x,next_y))
+
+    if((cur_X !=0 )):
+        next_x = cur_X-1
+        next_y = cur_y
+        total_board[next_x][next_y].determine_tile_safeness(array_of_pings)
+        print((total_board[next_x][next_y].safe))
+        if(total_board[next_x][next_y].safe ==True):
+            if (next_x,next_y) in safes:
+                print("is there")
+            else:
+                safes.append((next_x,next_y))
+    if((cur_y !=0 )):
+        next_x = cur_X
+        next_y = cur_y
+        total_board[next_x][next_y].determine_tile_safeness(array_of_pings)
+        print((total_board[next_x][next_y].safe))
+        if(total_board[next_x][next_y].safe ==True):
+            if (next_x,next_y) in safes:
+                print("is there")
+            else:
+                safes.append((next_x,next_y))
+    print(len(safes))
+
+
+
+i = 0
+while(i<len(safes)):
+    alocating_pings(safes[i])
+    print(len(safes))
+    next = i + 1
+    found_set = []
+    plot_movement(safes[i],safes[next],False,found_set)
+    for k in range(4):
+        currentline =""
+        for j in range(4):
+            pos = (j,k)
+            if pos in found_set:
+                currentline = "X"+" "+currentline
+            else:
+                currentline = " "+" "+currentline
+        print(currentline)
+
+    i+=1
